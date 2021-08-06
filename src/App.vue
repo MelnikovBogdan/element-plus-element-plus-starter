b49842a27c9e51c0014d1132c28be22d5c9ae621
MelnikovBogdan <maestro.25@mail.ru>
2021-08-06 23:33:57 +0300

    try

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <el-select
+      class="bg-red-500 border-2 border-red-500"
+      v-model="value"
+      multiple
+      filterable
+      remote
+      reserve-keyword
+      placeholder="Please enter a keyword"
+      :remote-method="remoteMethod"
+      :loading="loading">
+    <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+  return {
+  options: [{
+    value: 'HTML',
+    label: 'HTML'
+  }, {
+    value: 'CSS',
+    label: 'CSS'
+  }, {
+    value: 'JavaScript',
+    label: 'JavaScript'
+  }],
+  value: [],
+  list: [],
+  loading: false,
+  states: ["Alabama", "Alaska", "Arizona",
+  "Arkansas", "California", "Colorado",
+  "Connecticut", "Delaware", "Florida",
+  "Georgia", "Hawaii", "Idaho", "Illinois",
+  "Indiana", "Iowa", "Kansas", "Kentucky",
+  "Louisiana", "Maine", "Maryland",
+  "Massachusetts", "Michigan", "Minnesota",
+  "Mississippi", "Missouri", "Montana",
+  "Nebraska", "Nevada", "New Hampshire",
+  "New Jersey", "New Mexico", "New York",
+  "North Carolina", "North Dakota", "Ohio",
+  "Oklahoma", "Oregon", "Pennsylvania",
+  "Rhode Island", "South Carolina",
+  "South Dakota", "Tennessee", "Texas",
+  "Utah", "Vermont", "Virginia",
+  "Washington", "West Virginia", "Wisconsin",
+  "Wyoming"]
+}
+},
+  mounted() {
+  this.list = this.states.map(item => {
+  return { value: `value:${item}`, label: `label:${item}` };
+});
+},
+  methods: {
+  remoteMethod(query) {
+  if (query !== '') {
+  this.loading = true;
+  setTimeout(() => {
+  this.loading = false;
+  this.options = this.list.filter(item => {
+  return item.label.toLowerCase()
+  .indexOf(query.toLowerCase()) > -1;
+});
+}, 200);
+} else {
+  this.options = [];
+}
+}
+}
+}
+</script>
